963e9e0e6ec6b72653172f9d951f791e1bfab284
Oleksiy27 <91156768+Oleksiy27@users.noreply.github.com>
2022-01-13 15:05:33 +0200

    Create pavl1.php

---
+++ openserver1/pavl1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello</title>
+    <style>
+        form {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<form action="pavl.php" method="post">
+    <input type="text" id ="note" name="note"><br>
+    <p>Виберіть дату: <br> <input type="date" name="calendar"><br>
+        <input type="submit">
+</form>
+</body>
+</html>
